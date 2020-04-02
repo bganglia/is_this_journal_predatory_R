a6053b75377c1b463a5e113cd381daca3fe9dae9
bganglia <bganglia892@gmail.com>
2020-04-02 02:04:36 -0400

    basic structure for R package

---
+++ main.R
@@ -0,0 +1,45 @@
+get_domain <- function(url) {
+    url <- gsub("http://", "", url)
+    url <- gsub("https://", "", url)
+    slash_idx <- gregexp(pattern="/", url)[[1]]
+    if (length(slash_idx) == 0) {
+        domain = url
+    }
+    else {
+        domain = substr(url, 0, slash_idx)
+    }
+    domain
+}
+
+matches_domain <- function(predatory_name, text_descriptor)  {
+    any(get_domain(text_descriptor) == domains)
+}
+
+matches_name <- function(predatory_name, text_descriptor) {
+    any(tolower(predatory_name) == tolower(text_descriptor))
+}
+
+matches <- function(predatory_info, text_descriptor) {
+
+}
+
+format_predatory_string <- function(predatory_info) {
+
+}
+
+format_not_predatory <- function(text_descriptor) {
+
+}
+
+is_predatory <- function(text_descriptor) {
+    text_response = ""
+    for (predatory_info in predatory_info_sets) {
+        if (matches(predatory_info, text_descriptor)) {
+            text_response <- format_predatory_string(predatory_info)
+        }
+    }
+    if (text_response == "") {
+        text_response <- format_not_predatory(text_descriptor)
+    }
+    text_response
+}
