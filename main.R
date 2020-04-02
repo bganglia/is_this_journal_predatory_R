4a226a96ab8abb281b96c541a33186209b68beb1
bganglia <bganglia892@gmail.com>
2020-04-02 04:02:12 -0400

    bug fixes

--- main.R
+++ main.R
@@ -1,7 +1,11 @@
+library(rjson)
+
+setwd("~/Documents/proj/open_source/original/is_this_journal_predatory/is_this_journal_predatory_R")
+
 get_domain <- function(url) {
     url <- gsub("http://", "", url)
     url <- gsub("https://", "", url)
-    slash_idx <- gregexp(pattern="/", url)[[1]]
+    slash_idx <- gregexpr(pattern="/", url)[[1]]
     if (length(slash_idx) == 0) {
         domain = url
     }
@@ -12,22 +16,25 @@ get_domain <- function(url) {
 }
 
 matches_domain <- function(predatory_name, text_descriptor)  {
-    any(get_domain(text_descriptor) == domains)
+    any(sapply(predatory_name, get_domain) == get_domain(text_descriptor))
 }
 
 matches_name <- function(predatory_name, text_descriptor) {
+    #print(tolower(predatory_name))
+    #print(tolower(text_descriptor))
+    #print(any(tolower(predatory_name) == tolower(text_descriptor)))
     any(tolower(predatory_name) == tolower(text_descriptor))
 }
 
 matches <- function(predatory_info, text_descriptor) {
-    matches_domain(predatory_info$domain, text_descriptor) | matches_name(predatory_info$name, text_descriptor)
+    matches_domain(predatory_info$url, text_descriptor) | matches_name(predatory_info$name, text_descriptor)
 }
 
 format_predatory_string <- function (predatory_info) {
     sprintf(
         "The journal %s at %s is listed as a predatory journal in Beall's List",
         predatory_info$name[1],
-        predatory_info$name[2]
+        predatory_info$url[1]
     )
 }
 
@@ -40,7 +47,7 @@ format_not_predatory <- function(text_descriptor) {
 
 json_path <- "predatory_journals.json"
 
-predatory_info_sets <- fromJSON(file=json_path)
+predatory_info_sets <- fromJSON(file=json_path)[[1]]
 
 is_predatory <- function(text_descriptor) {
     text_response = ""
@@ -54,3 +61,5 @@ is_predatory <- function(text_descriptor) {
     }
     text_response
 }
+
+is_predatory("Academic Journals")
