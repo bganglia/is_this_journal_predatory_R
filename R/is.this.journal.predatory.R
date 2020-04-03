05fa5371842b0757087e9836cd16c337939cbb73
bganglia <bganglia892@gmail.com>
2020-04-03 04:28:30 -0400

    move code

---
+++ R/is.this.journal.predatory.R
@@ -0,0 +1,9 @@
+#' is.this.journal.predatory: A package for checking whether a journal is legitimate or predatory
+#'
+#' The is_predatory function, which accepts either the name or URL of a journal, will output 
+#' a sentence stating whether the journal is listed in Beall's List of predatory journals
+#' (beallslist.net).
+#'
+#' @docType package
+#' @name is.this.journal.predatory
+NULL
